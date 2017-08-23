8bcbc7545f286d4a21bc804d983ac19365984195
Adikota <aditya.kota@gmail.com>
2017-08-23 15:24:16 -0500

    Update WMI_DISKSPACE_MOUNTPOINTS.ps1

--- WMI_DISKSPACE_MOUNTPOINTS.ps1
+++ WMI_DISKSPACE_MOUNTPOINTS.ps1
@@ -1,10 +1,2 @@
-﻿Get-WmiObject Win32_LogicalDisk -filter "DriveType=3" -ComputerName RDBEDW01TS1,RDBEDW02TS1,RDBEDW03TS1| Select SystemName,DeviceID,VolumeName,@{Name="Size";Expression={"{0:N1}" -f($_.size/1gb)}},@{Name="FreeSpace";Expression={"{0:N1}"-f($_.freespace/1gb)}},@{Name="FreespacePcnt";Expression={"{0:N1}"-f((($_.freespace/1gb)/($_.size/1gb)*100.00))}} | Out-GridView
-
-
-
-Get-WmiObject Win32_LogicalDisk -filter "DriveType=3" -ComputerName RDBDDM06DEV| Select SystemName,DeviceID,VolumeName,@{Name="Size";Expression={"{0:N1}" -f($_.size/1gb)}},@{Name="FreeSpace";Expression={"{0:N1}"-f($_.freespace/1gb)}},@{Name="FreespacePcnt";Expression={"{0:N1}"-f((($_.freespace/1gb)/($_.size/1gb)*100.00))}} | Out-GridView
-
-
-
-Get-WmiObject Win32_LogicalDisk -filter "DriveType=3" -Credential $cred -ComputerName CLSOMA33ADEV.DEV.blueauth.com| Select SystemName,DeviceID,VolumeName,@{Name="Size";Expression={"{0:N1}" -f($_.size/1gb)}},@{Name="FreeSpace";Expression={"{0:N1}"-f($_.freespace/1gb)}},@{Name="FreespacePcnt";Expression={"{0:N1}"-f((($_.freespace/1gb)/($_.size/1gb)*100.00))}} | Out-GridView
+﻿Get-WmiObject Win32_LogicalDisk -filter "DriveType=3" -ComputerName ServerName1,ServerName2,ServerName3 | Select SystemName,DeviceID,VolumeName,@{Name="Size";Expression={"{0:N1}" -f($_.size/1gb)}},@{Name="FreeSpace";Expression={"{0:N1}"-f($_.freespace/1gb)}},@{Name="FreespacePcnt";Expression={"{0:N1}"-f((($_.freespace/1gb)/($_.size/1gb)*100.00))}} | Out-GridView
 
