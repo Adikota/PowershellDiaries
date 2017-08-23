302704f16735d1ef9d2ccb93d0c072b1e5ba37e6
Adikota <aditya.kota@gmail.com>
2017-08-23 15:22:10 -0500

    AllColumns_AllServers
    Column List from all Servers in your SQL environment

---
+++ AllColumns_AllServers.ps1
@@ -0,0 +1,24 @@
+﻿$servers = Invoke-Sqlcmd -ServerInstance DataMgmtDBPRD -Database DDMCMDB -Query "select InstanceName from MasterInstanceList"
+
+Invoke-SqlCmd -ServerInstance DataMgmtDBPRD -DatabaseName DataMgmt -Query "TRUNCATE TABLE ColumnList"
+
+foreach ($server in $servers)
+{
+   
+    $dbName = Invoke-Sqlcmd -ServerInstance $server.InstanceName -Database Master -Query "select name from sys.databases where database_id > 4 and name not in('DataMgmt')"
+
+    foreach($db in $dbName)
+    {
+      $coldata =  Invoke-Sqlcmd -ServerInstance $server.InstanceName -Database $db.name -Query "select @@SERVERNAME AS ServerName,DB_NAME() AS DBName,A.TABLE_NAME,COLUMN_NAME,DATA_TYPE
+                                                                                from INFORMATION_SCHEMA.columns A
+                                                                                JOIN INFORMATION_SCHEMA.TABLES B
+                                                                                on A.TABLE_SCHEMA = B.TABLE_SCHEMA
+                                                                                AND A.TABLE_NAME = B.TABLE_NAME
+                                                                                where B.TABLE_TYPE = 'BASE TABLE'"
+
+    Write-SqlTableData -ServerInstance DataMgmtDBPRD -DatabaseName DataMgmt -SchemaName dbo -TableName ColumnList -InputData $coldata
+
+
+    }
+
+}
---
+++ DACPAC_extract.ps1
@@ -0,0 +1,16 @@
+﻿#Set-ExecutionPolicy -ExecutionPolicy Unrestricted
+Set-Location C:
+
+$srvr = 'RDBSVC03DEV'
+$Db = 'ProviderEnterpriseServiceV2'
+
+
+add-type -path "C:\Program Files (x86)\Microsoft SQL Server\110\DAC\bin\Microsoft.SqlServer.Dac.dll"
+
+$setupFilePath = "C:\Program Files (x86)\Microsoft SQL Server\110\DAC\bin\sqlpackage.exe"
+ 
+$dest = 'C:\work\'+$srvr+$Db+'Prod.dacpac'
+
+#$cred = Get-Credential
+
+Start-Process $setupFilePath -ArgumentList "/Action:Extract /p:VerifyExtraction=False /SourceDatabaseName:$Db /SourceServerName:$srvr /TargetFile:$dest" #-Credential $cred
