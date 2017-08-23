d69b2bd5010b657aaac5e43a8e96f599c7c1e648
Adikota <aditya.kota@gmail.com>
2017-08-23 15:17:57 -0500

    First Version
    Reads Email and Saves the unread email attachments from Mailbox to a local Folder

---
+++ ReadMyMailBox.ps1
@@ -0,0 +1,40 @@
+﻿# Location to Save the outlook attachments
+$save = 'C:\output\'
+ 
+Add-type -assembly "Microsoft.Office.Interop.Outlook" | out-null
+
+$olFolders = "Microsoft.Office.Interop.Outlook.olDefaultFolders" -as [type]
+
+
+
+$outlook = new-object -comobject outlook.application
+
+$namespace = $outlook.GetNameSpace("MAPI")
+
+$folder = $namespace.getDefaultFolder($olFolders::olFolderInbox)
+
+$folItems = $folder.Items
+
+$folFilteredItems = $folItems.Restrict("[UnRead] = True")
+
+ForEach ($objMessage in $folFilteredItems)
+ {
+    $intCount = $objMessage.Attachments.Count
+    If ($intCount -gt 0) 
+    {
+        For ($i=1; $i -le $intCount; $i++) 
+        {
+            $objMessage.Attachments.Item($i).SaveAsFile($save +
+                $objMessage.Attachments.Item($i).FileName)
+        }
+    }
+    $objMessage.Unread = $false
+}
+
+
+#if you want to write the email content to the database but not the attachment since it is a COM object and to handle a comobject we need openrowset custom sql script
+
+#$Inputdata = $folder.items | Select-Object -Property SenderName,ReceivedTime,Subject,body | Select-Object -first 100 
+
+
+#Write-SqlTableData -ServerInstance RDBDDM06DEV -DatabaseName AlwaysEncrypted -SchemaName dbo -TableName EmailSave -InputData $Inputdata
